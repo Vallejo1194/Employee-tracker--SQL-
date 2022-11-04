e88cd2b4e1e7d80b09f7fe48dbeef02ac1c95f5c
Vallejo1194 <adrian.vallejo94@gmail.com>
2022-11-03 21:33:59 -0600

    seeds, .env, tables and database, dependencies

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO department_ (name)
+VALUES ("david"),
+     
+     INSERT INTO role_  (title, salary, department_id)
+VALUES ("senior", 45, 7)
+
+     INSERT INTO employee_  (first_name, last_name, role_id, manager_id)
+VALUES ("Juan", "Perez", 2, 5)
